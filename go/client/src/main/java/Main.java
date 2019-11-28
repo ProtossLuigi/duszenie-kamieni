6ce849f3e9f867852e1568a6fe426c8b4c9c5868
LazaniaPL <rysiu.piwowarski@onet.eu>
2019-11-29 00:55:52 +0100

    podział na moduły, okienko się wyświetla

--- go/client/src/main/java/Main.java
+++ go/client/src/main/java/Main.java
@@ -1,5 +1,21 @@
-public class Main {
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        System.out.println("Hello, world!");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+
+        Parent root = FXMLLoader.load(getClass().getResource("/fxml/startView.fxml"));
+        Scene scene = new Scene(root);
+        stage.setTitle("Go");
+        stage.setScene(scene);
+        stage.show();
     }
 }
